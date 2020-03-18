959af1430bc89f6f4a59c3a24698109ce34a9ba9
Kfir Zvi <zvikfir10@gmail.com>
2020-03-18 21:39:43 +0200

    changed home-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,39 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+
+    <ul>
+      {data.allStrapiArticle.edges.map(document => (
+        <li key={document.node.id}>
+          <h2>
+            <Link to={`/${document.node.Title}`}>{document.node.Title}</Link>
+          </h2>
+        </li>
+      ))}
+    </ul>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiArticle {
+      edges {
+        node {
+          id
+          Title
+          Content
+        }
+      }
+    }
+  }
+`
