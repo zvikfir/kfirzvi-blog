09745b17dfd8ce98bd5c13d60e47aa345f87ebf2
Kfir Zvi <zvikfir10@gmail.com>
2020-06-02 17:01:53 +0300

    changed article styling

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,6 +2,8 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import ReactMarkdown from "react-markdown"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { darcula } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 export const query = graphql`
   query ArticleQuery($id: Int!) {
@@ -16,10 +18,20 @@ export const query = graphql`
 const Article = ({ data }) => {
   const article = data.strapiArticle
 
+  const preTagRenderer = ({ language, value }) => (
+    <SyntaxHighlighter language={"python"} style={darcula} showLineNumbers>
+      {value}
+    </SyntaxHighlighter>
+  )
+
   return (
     <Layout>
-      {/* <ReactMarkdown source={article.content} unwrapDisallowed={true} disallowedTypes={['paragraph']}/> */}
-      <ReactMarkdown source={article.content} />
+      <ReactMarkdown
+        source={article.content}
+        renderers={{
+          code: preTagRenderer,
+        }}
+      />
 
       {/* <p>{article.content}</p> */}
     </Layout>
